441bb9e2b954ecb175a3056b40b3dd1370f2a3df
Edgar-Hdez94 <edgar-hdez94@hotmail.com>
2026-04-06 21:37:00 -0700

    Making changes to last week.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ st.write("Sube un archivo Excel o CSV con datos de outages para analizar circuit
 uploaded_file = st.file_uploader("Upload Excel or CSV file", type=["xlsx", "csv"])
 
 def load_file(file):
-    if file.name.endswith("xlsx"):
+    if file.name.endswith(".xlsx"):
         return pd.read_excel(file)
-    elif file.name.endswith("csv"):
+    elif file.name.endswith(".csv"):
         return pd.read_csv(file)
     return None
 
@@ -34,16 +34,11 @@ if uploaded_file is not None:
         df = load_file(uploaded_file)
         df = clean_columns(df)
 
- 
-
         st.subheader("Preview of uploaded data")
         st.dataframe(df.head())
 
- 
-
         required_cols = ["Substation", "Feeder", "Outage #", "SAIDI", "Customers Out", "Duration"]
 
- 
 
         missing = [col for col in required_cols if col not in df.columns]
 
